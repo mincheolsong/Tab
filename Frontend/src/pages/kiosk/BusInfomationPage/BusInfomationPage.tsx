8ab65cf3d1f9a754160019e305cbca9ed0ca814d
cocosasa <wjdgns0631@gmail.com>
2023-08-01 01:19:27 +0900

    feat:dust_api

--- Frontend/src/pages/kiosk/BusInfomationPage/BusInfomationPage.tsx
+++ Frontend/src/pages/kiosk/BusInfomationPage/BusInfomationPage.tsx
@@ -5,9 +5,8 @@ import { ComingSoonBusList } from '../../../components/kiosk/ComingSoonBusList';
 import { ArrivalBusList } from '../../../components/kiosk/ArrivalBusList';
 import { LivingInformationBox } from '../../../components/kiosk/LivingInfomationBox';
 import { BottomButtonBox } from '../../../components/kiosk/BottomButtonBox';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { useEffect, useRef, useState } from 'react';
-import { delay } from '@reduxjs/toolkit/dist/utils';
 
 export interface BusData {
   remainingStops: number; 
@@ -23,22 +22,9 @@ export interface BusData {
 }
 
 export const BusInfomationPage: FC<BusInfomationPageProps> = (props) => {
-  const citycode = 37050;
-  const busStopId = 'GMB383';
-	const options: object = {
-    url: "http://192.168.100.119/api/stops/37050/GMB383",
-    // url: `http://192.168.100.119/api/stops/${citycode}/${busStopId}`,
-    method: "GET",
-    timeout: 10000,
-    headers: {
-      "Authorization": "Bearer YOUR_ACCESS_TOKEN",
-      "Content-Type": "application/json", // 원하는 Content-Type을 설정할 수 있습니다.
-      // 다른 헤더도 필요에 따라 추가할 수 있습니다.
-    },
-  };
 
 
-	const [data, setData] = useState<BusData[]>([]);
+	const [busData, setBusData] = useState<BusData[]>([]);
 
   function useInterval(callback: () => void, delay: number | null) {
     const savedCallback = useRef<typeof callback>(callback);
@@ -60,28 +46,30 @@ export const BusInfomationPage: FC<BusInfomationPageProps> = (props) => {
     }, [delay]);
   }
 
-  function updateData() {
-    axios(options)
-      .then((response) => {
-        setTimeout(5000);
-        console.log(response);
-        
-        // setData(response);
-      })
-      .catch(err=>{
-        console.log(err);
-        
-      })
-  }
+  const updateBusData = async () => {
 
-	useInterval(updateData, 30000)
+    try {
+      const citycode: number = 37050;
+      const busStopId: string = "GMB383";
+
+      const url = `http://192.168.100.119/api/stops/${citycode}/${busStopId}`;
+
+      const response: AxiosResponse<BusData[]> = await axios.get(url);
+      setBusData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching buslist data:", error);
+    }
+  };
+
+	useInterval(updateBusData, 30000);
 
   return (
     <div {...props}>
       <Header />
-      <ComingSoonBusList/>
-      <ArrivalBusList data={data} />
-      <LivingInformationBox/>
+      <ComingSoonBusList />
+      <ArrivalBusList data={busData} />
+      <LivingInformationBox />
       <BottomButtonBox />
     </div>
   );
